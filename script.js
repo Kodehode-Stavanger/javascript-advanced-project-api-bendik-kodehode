e18b8fe698fe685058eb4fc6dcdb9504af667202
Bendik Kodehode <bendik08.kodehode@gmail.com>
2024-02-19 22:35:00 +0100

    Testing out different approaches.

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-async function getData() {
+const getInfo =  "https://v2.jokeapi.dev/info";
+const getJoke = " https://v2.jokeapi.dev/joke/";
+
+const optionsForm = document.getElementById("options-form");
+
+
+
+async function getData(link) {
     try {
-        const result = await fetch("/api/v2/facts/random");
+        const result = await fetch(`${link}`);
         const data = await result.json();
         renderSite(data);
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
 
-getData();
+getData(getInfo);
 
 function renderSite(data) {
+    generateCheckboxOptions(data)
     console.log(data)
+}
+
+function generateCheckboxOptions(data) {
+    const categoryList = data.jokes.categories;
+    const flagList = data.jokes.flags;
+    const typeList = data.jokes.types;
+
+    const fieldset = document.createElement("fieldset");
+    const title = document.createElement("legend");
+
+    fieldset.classList.add(`${title.toLowerCase}`)
 }
